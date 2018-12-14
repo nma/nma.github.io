2ba3dbac9f6e4051e734498603a0c35d7af5b968
nma <nickma38@gmail.com>
2018-12-13 23:19:27 -0500

    gatsby initial setup

---
+++ nma.github.io/src/components/layout.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+import { ThemeProvider } from 'styled-components'
+import { themes } from 'toxin-ui';
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]} 
+        >
+          <html lang="en" />
+          <link href="https://fonts.googleapis.com/css?family=Quicksand|Roboto|Montserrat" rel="stylesheet"></link>
+        </Helmet>
+        <ThemeProvider theme={themes.toxin}>
+          <div>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '0px 1.0875rem 1.45rem',
+                paddingTop: 0,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </ThemeProvider>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
