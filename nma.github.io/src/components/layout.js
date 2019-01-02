7fc6a26ce91cd4b6d7f9b0279bf36c7caa3d4452
nma <nickma38@gmail.com>
2019-01-02 00:25:26 -0500

    use gatsby blog starter

--- nma.github.io/src/components/layout.js
+++ nma.github.io/src/components/layout.js
@@ -1,58 +1,57 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
+import { Head1, Head3, themes } from 'toxin-ui'
 import { ThemeProvider } from 'styled-components'
-import { themes } from 'toxin-ui';
 
-import Header from './header'
-import './layout.css'
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]} 
-        >
-          <html lang="en" />
-          <link href="https://fonts.googleapis.com/css?family=Quicksand|Roboto|Montserrat" rel="stylesheet"></link>
-        </Helmet>
-        <ThemeProvider theme={themes.toxin}>
-          <div>
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <div
-              style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '0px 1.0875rem 1.45rem',
-                paddingTop: 0,
-              }}
-            >
-              {children}
-            </div>
-          </div>
-        </ThemeProvider>
-      </>
-    )}
-  />
-)
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    if (location.pathname === rootPath) {
+      header = (
+        <Head1>
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </Head1>
+      )
+    } else {
+      header = (
+        <Head3>
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </Head3>
+      )
+    }
+    return (
+      <ThemeProvider theme={themes.toxin}>
+        <div>
+          {header}
+          {children}
+          <footer>
+            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default Layout
--- nma.github.io/src/pages/index.js
+++ nma.github.io/src/pages/index.js
@@ -1,19 +1,65 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+import Bio from '../components/Bio'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+import { Head3 } from 'toxin-ui'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
 
-export default IndexPage
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="All posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <Head3>
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </Head3>
+              <small>{node.frontmatter.date}</small>
+              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
